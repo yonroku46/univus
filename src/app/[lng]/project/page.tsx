d97d9f50519fcb4de83eac67c364b0055dc6be58
4ROKU <71884535+yonroku46@users.noreply.github.com>
2025-03-12 20:48:20 +0900

    update project page

--- src/app/[lng]/project/page.tsx
+++ src/app/[lng]/project/page.tsx
@@ -3,12 +3,14 @@
 import { useEffect, useState } from 'react';
 import { AvailableLanguages } from '@/i18n/settings';
 import { useTranslation } from '@/i18n/client';
+import Image from 'next/image';
 
 import InsightsOutlinedIcon from '@mui/icons-material/InsightsOutlined';
 import AddBusinessOutlinedIcon from '@mui/icons-material/AddBusinessOutlined';
 import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined';
 import LocalDiningOutlinedIcon from '@mui/icons-material/LocalDiningOutlined';
 import TaskAltOutlinedIcon from '@mui/icons-material/TaskAltOutlined';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 export default function ServiceInfoPage(
   { params: { lng } }: { params: { lng: AvailableLanguages } }
@@ -38,6 +40,12 @@ export default function ServiceInfoPage(
     { icon: LocalDiningOutlinedIcon, title: t('project.service.buyer.benefit.item1'), description: t('project.service.buyer.benefit.description1') },
     { icon: TaskAltOutlinedIcon, title: t('project.service.buyer.benefit.item2'), description: t('project.service.buyer.benefit.description2') },
     { icon: ThumbUpOffAltOutlinedIcon, title: t('project.service.buyer.benefit.item3'), description: t('project.service.buyer.benefit.description3') }
+  ];
+
+  const usageCards = [
+    { title: t('project.service.usage.item1'), description: t('project.service.usage.description1'), img: "/assets/img/usage-select.png" },
+    { title: t('project.service.usage.item2'), description: t('project.service.usage.description2'), img: "/assets/img/usage-order.png" },
+    { title: t('project.service.usage.item3'), description: t('project.service.usage.description3'), img: "/assets/img/usage-takeout.png" },
   ];
 
   return (
@@ -74,6 +82,13 @@ export default function ServiceInfoPage(
       <div className='project-detail'>
         <div className='container'>
 
+          <div className='service-description'>
+            <h2>{t('project.service.detail.title')}</h2>
+            <div className='contents'>
+              {t('project.service.detail.content')}
+            </div>
+          </div>
+
           <div className='service-benefit'>
             <h2>{userType === 'seller' ? t('project.service.seller.benefit.title') : t('project.service.buyer.benefit.title')}</h2>
             <div className='card'>
@@ -92,14 +107,31 @@ export default function ServiceInfoPage(
           </div>
 
           <div className='service-method'>
-            <h2>{t('project.service.method.title')}</h2>
-            <div className='coming-soon'>
-              <div className='sub-title'>
-                Coming Soon
-              </div>
-              <div className='title'>
-                {t('project.comingSoon')}
-              </div>
+            <h2>{t('project.service.usage.title')}</h2>
+            <div className="service-usage">
+              {usageCards.map((card, index) => (
+                <>
+                  <div key={index} className="usage-card">
+                    <div className="title-wrapper">
+                      <div className="title">
+                        {card.title}
+                      </div>
+                      <Image
+                        src={card.img}
+                        alt={card.title}
+                        width={110}
+                        height={110}
+                      />
+                    </div>
+                    <div className="description">
+                      {card.description}
+                    </div>
+                  </div>
+                  {index < usageCards.length - 1 && (
+                    <ArrowRightIcon className="usage-card-arrow" />
+                  )}
+                </>
+              ))}
             </div>
           </div>
 
