046f27d30d5b8168131b7e2595c0598e70e31cd3
4ROKU <71884535+yonroku46@users.noreply.github.com>
2025-03-28 10:55:09 +0900

    next-sitemap add

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const sitemapConfig = {
+  siteUrl: 'https://univus.co.jp',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: [
+    '/api/*',
+    '/admin/*',
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/']
+      }
+    ],
+    additionalSitemaps: [
+      'https://univus.co.jp/server-sitemap.xml'
+    ]
+  }
+};
+
+export default sitemapConfig;
